bd5143cefb88126a9779f267a334c351d920c608
quanwenchao <quan_wc@163.com>
2018-05-15 21:14:29 +0800

    根据前序、中序遍历结果，重建二叉树

--- src/com/gelonghui/entity/BTree.java
+++ src/com/gelonghui/entity/BTree.java
@@ -1,8 +1,68 @@
 package com.gelonghui.entity;
 
 /**
+ * 二叉树实体类
  * Created by quanwenchao
- * 2018/5/8 19:32:43
+ * 2018/5/15 09:48:43
  */
 public class BTree {
+    /** 数值域 */
+    private Integer data;
+    /** 左孩子 */
+    private BTree left;
+    /** 右孩子 */
+    private BTree right;
+
+    /**
+     * 无参构造
+     */
+    public BTree() {
+
+    }
+
+    public BTree(Integer data) {
+        this.data = data;
+    }
+
+    /**
+     * 有参构造
+     */
+    public BTree(Integer data, BTree left, BTree right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public Integer getData() {
+        return data;
+    }
+
+    public void setData(Integer data) {
+        this.data = data;
+    }
+
+    public BTree getLeft() {
+        return left;
+    }
+
+    public void setLeft(BTree left) {
+        this.left = left;
+    }
+
+    public BTree getRight() {
+        return right;
+    }
+
+    public void setRight(BTree right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "BTree{" +
+                "data=" + data +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
 }
---
+++ src/com/gelonghui/service/create/ConstructTree.java
@@ -0,0 +1,24 @@
+package com.gelonghui.service.create;
+
+/**
+ * 要求：
+ *      输入某二叉树的前序遍历和中序遍历的结果，重建出该二叉树。
+ *      假设输入的前序遍历和中序遍历的结果中都不包含重复的数字。
+ *
+ * 实现：
+ *
+ * 例子：
+ *      前序遍历序列：{1, 2, 4, 7, 3, 5, 6, 8}
+ *      中序遍历序列：{4, 7, 2, 1, 5, 3, 8, 6}
+ *
+ * Created by quanwenchao
+ * 2018/5/15 20:27:35
+ */
+public class ConstructTree {
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
