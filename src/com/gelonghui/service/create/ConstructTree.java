94f6f751119dbd6f858bdf8bb76251072d6fd883
quanwenchao <quan_wc@163.com>
2018-05-17 20:48:33 +0800

    前序、中序构建二叉树，初步实现思路

--- src/com/gelonghui/service/create/ConstructTree.java
+++ src/com/gelonghui/service/create/ConstructTree.java
@@ -1,5 +1,7 @@
 package com.gelonghui.service.create;
 
+import com.gelonghui.entity.BTree;
+
 /**
  * 要求：
  *      输入某二叉树的前序遍历和中序遍历的结果，重建出该二叉树。
@@ -12,13 +14,55 @@ package com.gelonghui.service.create;
  *      中序遍历序列：{4, 7, 2, 1, 5, 3, 8, 6}
  *
  * Created by quanwenchao
- * 2018/5/15 20:27:35
+ * 2018/5/17 20:27:35
  */
 public class ConstructTree {
 
 
+    /**
+     * 递归构建二叉树
+     * @return
+     */
+    public static BTree constructTree(Integer[] startPreOrder, Integer endPreOrder, Integer[] startInOrder, Integer endInOrder) {
+
+        // step1：前序遍历序列第一个数字是树的根节点
+        Integer rootValue = startPreOrder[0];
+        BTree root = new BTree();
+        root.setData(rootValue);
+        root.setLeft(null);
+        root.setRight(null);
+
+        // step2：在中序遍历序列中找到根节点
+        Integer rootInOrder = startInOrder[0];
+        while (rootInOrder <= endInOrder && startInOrder[rootInOrder] != rootValue) {
+            ++rootInOrder;
+        }
+
+        //Integer leftLength = rootInOrder - startInOrder;
+        Integer leftLength = rootInOrder - 0;
+
+        //Integer leftPreOrderEnd = startPreOrder + leftLength;
+        Integer leftPreOrderEnd = 0 + leftLength;
+
+        if (leftLength > 0) {
+            // 构建左子树
+            root.setLeft(constructTree(startPreOrder, leftPreOrderEnd, startInOrder, rootInOrder -1));
+        }
+
+        //if (leftLength < endPreOrder - startPreOrder) {
+        //    root.setRight(constructTree(leftPreOrderEnd+1, endPreOrder, rootInOrder + 1, endInOrder));
+        //}
+
+        return root;
+    }
+
+
 
     public static void main(String[] args) {
+
+        Integer[] preOrder = new Integer[]{1, 2, 4, 7, 3, 5, 6, 8};
+        Integer[] inOrder = new Integer[]{4, 7, 2, 1, 5, 3, 8, 6};
+
 
     }
 }
