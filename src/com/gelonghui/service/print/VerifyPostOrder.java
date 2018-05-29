9cd4b46503860929779bfc4e4d88b040c1272bbf
quanwenchao <quan_wc@163.com>
2018-05-29 22:44:01 +0800

    判断数组内容是不是某二元查找树的后序遍历的结果

--- src/com/gelonghui/service/print/VerifyPostOrder.java
+++ src/com/gelonghui/service/print/VerifyPostOrder.java
@@ -1,5 +1,8 @@
 package com.gelonghui.service.print;
 
+import com.gelonghui.entity.BTree;
+import com.gelonghui.service.create.CreateFindTree;
+
 /**
  * 要求：
  *      输入一个整数数组，判断该数组是不是某二元查找树的后序遍历的结果。如果是返回true，否则返回false。
@@ -13,9 +16,27 @@ package com.gelonghui.service.print;
  */
 public class VerifyPostOrder {
 
+    public static Boolean compute(BTree root, Integer[] arr) {
 
+        boolean flag = false;
+
+
+        return flag;
+    }
 
     public static void main(String[] args) {
+
+        Integer[] arrData = new Integer[]{6, 2, 5, 8};
+        Integer[] arrParam = new Integer[]{6, 2, 5, 8};
+
+        //CreateFindTree obj = new CreateFindTree();
+        //obj.createFindTree(arr); // 对象调用静态的方法，为什么不可以？
+
+        BTree root = CreateFindTree.createFindTree(arrData);
+
+        Boolean flag = compute(root, arrParam);
+        System.out.println("flag: " + flag);
+
 
     }
 }
